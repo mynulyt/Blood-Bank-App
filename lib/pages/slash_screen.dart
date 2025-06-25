571417ea55f6384843f138393505b32aadbc3714
Mynul Alam <mynulalam27@gmail.com>
2025-06-26 01:11:46 +0600

    c

--- lib/pages/slash_screen.dart
+++ lib/pages/slash_screen.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
 
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    });
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/logo.png', height: 100),
+            SizedBox(height: 20),
+            Text("Blood Management App", style: TextStyle(fontSize: 20)),
+          ],
+        ),
+      ),
+    );
+  }
+}
