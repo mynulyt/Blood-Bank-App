70a62cb19d47d31c8fa9b1074140d59875a7ee2e
Mynul Alam <mynulalam27@gmail.com>
2025-06-26 01:04:46 +0600

    c

---
+++ lib/pages/request_blood_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class RequestBloodPage extends StatelessWidget {
+  final nameController = TextEditingController();
+  final groupController = TextEditingController();
+  final locationController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Request Blood")),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Patient Name'),
+            ),
+            TextField(
+              controller: groupController,
+              decoration: InputDecoration(labelText: 'Blood Group'),
+            ),
+            TextField(
+              controller: locationController,
+              decoration: InputDecoration(labelText: 'Location'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed:
+                  () => ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(SnackBar(content: Text("Request Submitted!"))),
+              child: Text("Submit Request"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
