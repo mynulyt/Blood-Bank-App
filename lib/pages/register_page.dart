c8f87b8954e4f3da9157e23f2577c4228809ab28
Mynul Alam <mynulalam27@gmail.com>
2025-06-26 01:07:40 +0600

    c

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class RegisterPage extends StatelessWidget {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Register")),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed:
+                  () => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (_) => HomePage()),
+                  ),
+              child: Text("Register"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
