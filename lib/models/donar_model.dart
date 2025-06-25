c509230b932e333159079e084a7158197c2a4091
Mynul Alam <mynulalam27@gmail.com>
2025-06-26 00:54:37 +0600

    c

---
+++ lib/models/donar_model.dart
@@ -0,0 +1,11 @@
+class DonorModel {
+  final String name;
+  final String bloodGroup;
+  final String location;
+
+  DonorModel({
+    required this.name,
+    required this.bloodGroup,
+    required this.location,
+  });
+}
